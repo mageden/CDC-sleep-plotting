910de7b7a923ae27de1db5d8da3951c55adaabf3
mgeden <mageden@ncsu.edu>
2018-06-26 19:04:46 -0400

    Add in freq/miss plots for state level

--- 2016_missing.R
+++ 2016_missing.R
@@ -1,6 +1,6 @@
 
 # ==============================================================================
-#             Contintinuous and Dichotomized Hours Slept
+#             Frequency of Response and Proportion Responded
 # ==============================================================================
 
 # ---- Setup ----
@@ -18,7 +18,7 @@ theme_map <- theme_bw() +
           panel.border = element_blank(),
           panel.grid = element_blank(),
           axis.title = element_blank(),
-          legend.position = c(.95, .2),
+          legend.position = c(.96, .2),
           legend.title = element_blank(),
           legend.text = element_text(size = 10),
           legend.margin=margin(t=0, r=0, b=0, l=0, unit="cm"),
@@ -27,42 +27,40 @@ theme_map <- theme_bw() +
           plot.margin = unit(c(0.1,0,0.1,0.1), "lines"))
 us_map <- map_data("state")
 
-# Continuous Hours Slept
-cont <- ggplot() +
+# Frequency
+freq <- ggplot() +
     geom_map(data=us_map, map=us_map,
              aes(long, lat, map_id=region),
              color="#b2b2b277", size=0.1, fill=NA) +
     geom_map(data=sleep_2016_cont, map=us_map,
-             aes(fill=SLEPTIM1.f, map_id=polyname),
+             aes(fill=n, map_id=polyname),
              color="#b2b2b277", size=0.4) +
-    ggtitle("Average Hours Slept") +
-    scale_fill_distiller(name="Prevalence", palette="BrBG") +
+    ggtitle("Sample Size") +
+    scale_fill_distiller(name="Prevalence", palette="BrBG",
+                         breaks=c(10000,20000,30000),
+                         labels=c("10k","20k","30k")) +
     coord_map("polyconic") +
     scale_x_continuous(limits = c(-118,-75)) +
     scale_y_continuous(limits = c(25.5,49.38),expand = c(0,0)) +
     theme(axis.title = element_blank())+
     theme_map
-
-ggsave(cont, file = "/home/michael/Code/Website/mgeden.github.io/images/sleepCDC/state_cont_plot.png",
+ggsave(freq, file = "/home/michael/Code/Website/mgeden.github.io/images/sleepCDC/state_freq_plot.png",
        height=2.6, width = 4, units = "in", dpi = 400)
 
-# DIchotomized Proportion Sleep Deprived
-sleep_2016_dichot$SLEPTIM1.f2 <- round(sleep_2016_dichot$SLEPTIM1.f,digits =2)
-dichot <- ggplot() +
+# Proportion Missing
+prop.resp <- ggplot() +
     geom_map(data=us_map, map=us_map,
              aes(long, lat, map_id=region),
              color="#b2b2b277", size=0.1, fill=NA) +
-    geom_map(data=sleep_2016_dichot, map=us_map,
-             aes(fill=SLEPTIM1.f2, map_id=polyname),
+    geom_map(data=sleep_2016_cont, map=us_map,
+             aes(fill=prop.responded, map_id=polyname),
              color="#b2b2b277", size=0.4) +
-    ggtitle("Proportion Sleep Deprived") +
-    scale_fill_distiller(name="Prevalence", palette="BrBG",labels=percent) +
+    ggtitle("Proportion Responded") +
+    scale_fill_distiller(name="Prevalence", palette="BrBG", label = percent) +
     coord_map("polyconic") +
     scale_x_continuous(limits = c(-118,-75)) +
     scale_y_continuous(limits = c(25.5,49.38),expand = c(0,0)) +
-    #scale_fill_gradient(labels = percent) +
     theme(axis.title = element_blank())+
     theme_map
-
-ggsave(dichot, file = "/home/michael/Code/Website/mgeden.github.io/images/sleepCDC/state_dichot_plot.png",
+ggsave(prop.resp, file = "/home/michael/Code/Website/mgeden.github.io/images/sleepCDC/state_resp_plot.png",
        height=2.6, width = 4, units = "in", dpi = 400)
